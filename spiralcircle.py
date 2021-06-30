e69fa4c433c679392ec1c43a70aae3cf234947bb
Rajsri07 <85505657+Rajsri07@users.noreply.github.com>
2021-06-30 20:27:18 +0530

    Add files via upload

---
+++ spiralcircle.py
@@ -0,0 +1,5 @@
+import turtle
+cir=turtle.Turtle()
+for i in range(11):
+    cir.circle(i*10)
+turtle.done()
---
+++ triangle.py
@@ -0,0 +1,6 @@
+import turtle
+tri=turtle.Turtle()
+for i in range(3):
+    tri.forward(100)
+    tri.left(120)
+turtle.done()
